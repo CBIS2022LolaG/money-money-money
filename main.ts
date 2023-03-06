ee67089f43b0ab384d4922cf19f3ee1ed2213499
CBIS2022LolaG <123550633+CBIS2022LolaG@users.noreply.github.com>
2023-03-06 15:30:50 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    music.setTempo(120)
+    for (let index = 0; index < 20; index++) {
+        music.rest(music.beat(BeatFraction.Quarter))
+    }
+    if (true) {
+    	
+    } else {
+    	
+    }
+})
